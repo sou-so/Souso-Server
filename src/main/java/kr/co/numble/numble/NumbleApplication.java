75c03a636c151fa931caf9b9afe9705f93df238f
Sangjin <sangjin170202@naver.com>
2022-10-26 20:12:05 +0900

    chore: 프로젝트 설정

---
+++ src/main/java/kr/co/numble/numble/NumbleApplication.java
@@ -0,0 +1,13 @@
+package kr.co.numble.numble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumbleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumbleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/numble/numble/NumbleApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.numble.numble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumbleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
