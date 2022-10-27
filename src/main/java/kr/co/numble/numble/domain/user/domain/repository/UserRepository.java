e3a10a42a004ca0b7490d2b15ec17feab3b4eb68
Sangjin <sangjin170202@naver.com>
2022-10-27 12:13:34 +0900

    feat: UserRepository 추가

---
+++ src/main/java/kr/co/numble/numble/domain/user/domain/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package kr.co.numble.numble.domain.user.domain.repository;
+
+import kr.co.numble.numble.domain.user.domain.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
