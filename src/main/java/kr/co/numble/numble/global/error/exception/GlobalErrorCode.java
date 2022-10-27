89cae9a5bb26c676df69d4611b2acfe860503993
Sangjin <sangjin170202@naver.com>
2022-10-27 12:03:39 +0900

    chore: GlobalErrorCode 추가

---
+++ src/main/java/kr/co/numble/numble/global/error/exception/GlobalErrorCode.java
@@ -0,0 +1,30 @@
+package kr.co.numble.numble.global.error.exception;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
+public enum GlobalErrorCode {
+    // 401
+    EXPIRED_JWT(401, "Expired Jwt"),
+    INVALID_JWT(401, "Invalid Jwt"),
+
+    // 403
+    FORBIDDEN(403, "FORBIDDEN"),
+    NOT_VALID_USER_ROLE(403, "Not Valid User Role"),
+
+    // 404
+    USER_NOT_FOUND(404, "User Not Found"),
+
+    // 409
+    ALREADY_USER_EXIST(409, "Already User Exist"),
+
+    // 500
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error");
+
+    private final int status;
+    private final String message;
+}
