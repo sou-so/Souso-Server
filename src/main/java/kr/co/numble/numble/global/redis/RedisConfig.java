b562894d2f594cc5ec05997195f2790bbef74be0
Sangjin <sangjin170202@naver.com>
2022-10-27 12:16:04 +0900

    chore: RedisConfig

---
+++ src/main/java/kr/co/numble/numble/global/redis/RedisConfig.java
@@ -0,0 +1,75 @@
+package kr.co.numble.numble.global.redis;
+
+import kr.co.numble.numble.global.property.redis.RedisProperties;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableCaching
+public class RedisConfig {
+    private final RedisProperties redisProperties;
+
+    @Bean
+    @ConditionalOnMissingBean(RedisConnectionFactory.class)
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+
+        return redisTemplate;
+    }
+
+    @Bean
+    public ZSetOperations<String, Object> zSetOperations() {
+        return redisTemplate().opsForZSet();
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        var stringSerializationPair = RedisSerializationContext
+                .SerializationPair.fromSerializer(new StringRedisSerializer());
+        var objectSerializationPair = RedisSerializationContext
+                .SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer());
+
+        var redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(stringSerializationPair)
+                .serializeValuesWith(objectSerializationPair)
+                .entryTtl(CachingDuration.HOUR.duration);
+
+        return RedisCacheManager.RedisCacheManagerBuilder
+                .fromConnectionFactory(redisConnectionFactory())
+                .cacheDefaults(redisCacheConfiguration).build();
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    enum CachingDuration {
+        DAY(Duration.ofDays(1L)),
+        HOUR(Duration.ofHours(1L));
+
+        private final Duration duration;
+    }
+}
