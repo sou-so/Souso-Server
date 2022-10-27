4ed3344ebaa30b923cd5dc41b6968281c78a0c82
Sangjin <sangjin170202@naver.com>
2022-10-27 12:28:46 +0900

    chore: WebMvcConfig 추가

---
+++ src/main/java/kr/co/numble/numble/global/config/WebMvcConfig.java
@@ -0,0 +1,17 @@
+package kr.co.numble.numble.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*");
+    }
+}
