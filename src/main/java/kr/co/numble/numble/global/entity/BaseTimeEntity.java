de680568702fb2e7eee893a2f96e8e673741e9cb
Sangjin <sangjin170202@naver.com>
2022-10-27 12:12:51 +0900

    feat: BaseTimeEntity 추가

---
+++ src/main/java/kr/co/numble/numble/global/entity/BaseTimeEntity.java
@@ -0,0 +1,22 @@
+package kr.co.numble.numble.global.entity;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.ZonedDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @CreatedDate
+    private final ZonedDateTime createdAt = ZonedDateTime.now();
+
+    @LastModifiedDate
+    private final ZonedDateTime modifiedAt = ZonedDateTime.now();
+}
