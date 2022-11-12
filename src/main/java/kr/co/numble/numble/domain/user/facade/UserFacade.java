65e083ab7f21c6261d5ddafff460ee6120fe788b
Sangjin <sangjin170202@naver.com>
2022-11-12 20:29:38 +0900

    feat: UserFacade 추가

---
+++ src/main/java/kr/co/numble/numble/domain/user/facade/UserFacade.java
@@ -0,0 +1,43 @@
+package kr.co.numble.numble.domain.user.facade;
+
+import kr.co.numble.numble.domain.user.domain.User;
+import kr.co.numble.numble.domain.user.domain.repository.UserRepository;
+import kr.co.numble.numble.domain.user.exception.AlreadyNicknameExistException;
+import kr.co.numble.numble.global.exception.AlreadyUserExistException;
+import kr.co.numble.numble.global.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class UserFacade {
+    private final UserRepository userRepository;
+
+    public User getCurrentUser() {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        return getUserByEmail(email);
+    }
+
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+    }
+
+    public User getUserById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+    }
+
+    public void checkUserExists(String email) {
+        if (userRepository.findByEmail(email).isPresent()) {
+            throw AlreadyUserExistException.EXCEPTION;
+        }
+    }
+
+    public void checkNicknameExists(String nickname) {
+        if (userRepository.findByNickname(nickname).isPresent()) {
+            throw AlreadyNicknameExistException.EXCEPTION;
+        }
+    }
+}
