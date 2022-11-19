ab374f3d598ac7b22f961541154c487aa14942f5
dkswnkk <jjoy9810@gmail.com>
2022-11-20 02:29:30 +0900

    chore: coolsms 관련 빌드 및 설정 업무 수행

---
+++ src/main/java/kr/co/numble/numble/infrastructure/sms/coolsms/CoolSmsProperties.java
@@ -0,0 +1,18 @@
+package kr.co.numble.numble.infrastructure.sms.coolsms;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@Getter
+@RequiredArgsConstructor
+@ConstructorBinding
+@ConfigurationProperties(prefix = "coolsms")
+public class CoolSmsProperties {
+
+    private final String key;
+    private final String secret;
+    private final String phoneNumber;
+
+}
