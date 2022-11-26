dc00981b251774771a052bf6d4a1b6416f3ac426
Sangjin <sangjin170202@naver.com>
2022-11-26 20:15:16 +0900

    refactor(#91): Convention 통일

--- src/main/java/kr/co/souso/souso/infrastructure/image/DefaultImage.java
+++ src/main/java/kr/co/souso/souso/infrastructure/image/DefaultImage.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class DefaultImage {
+
     public static final String USER_PROFILE_IMAGE = "https://souso-bucket.s3.ap-northeast-2.amazonaws.com/logo.svg";
 
 }
