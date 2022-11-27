b5ac985f3a0552e073f59d5fffb3b534f8b6446e
Sangjin <sangjin170202@naver.com>
2022-11-27 21:34:55 +0900

    fix: 오타 수정 및 값 보기 편하게 명시

--- src/main/java/kr/co/souso/souso/domain/user/presentation/dto/request/UpdatePasswordRequest.java
+++ src/main/java/kr/co/souso/souso/domain/user/presentation/dto/request/UpdatePasswordRequest.java
@@ -11,14 +11,14 @@ import javax.validation.constraints.Pattern;
 @NoArgsConstructor
 public class UpdatePasswordRequest {
 
-    @ApiModelProperty(value = "password", example = "souso1234#")
+    @ApiModelProperty(value = "기존 비밀번호", example = "souso1234#")
     @NotBlank(message = "password는 Null, 공백, 띄어쓰기를 허용하지 않습니다.")
     @Pattern(regexp = "(?=.*[a-z])(?=.*[0-9])(?=.*[!#$%&'()*+,./:;<=>?@＼^_`{|}~])[a-zA-Z0-9!#$%&'()*+,./:;" +
             "<=>?@＼^_`{|}~]{8,60}$",
-            message = "new_password는 소문자, 숫자, 특수문자가 포함되고 8자 이상이어야 합니다.")
+            message = "password는 소문자, 숫자, 특수문자가 포함되고 8자 이상이어야 합니다.")
     private String password;
 
-    @ApiModelProperty(value = "newPassword", example = "souso12345!")
+    @ApiModelProperty(value = "새로운 비밀번호", example = "souso12345!")
     @NotBlank(message = "new_password는 Null, 공백, 띄어쓰기를 허용하지 않습니다.")
     @Pattern(regexp = "(?=.*[a-z])(?=.*[0-9])(?=.*[!#$%&'()*+,./:;<=>?@＼^_`{|}~])[a-zA-Z0-9!#$%&'()*+,./:;" +
             "<=>?@＼^_`{|}~]{8,60}$",
