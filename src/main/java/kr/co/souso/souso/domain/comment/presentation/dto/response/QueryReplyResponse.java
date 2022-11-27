d9cc941357012db51e0bf3bf3b61b1e53c97421b
dkswnkk <jjoy9810@gmail.com>
2022-11-27 22:52:19 +0900

    feat: 댓글 관련 Response DTO 추가

---
+++ src/main/java/kr/co/souso/souso/domain/comment/presentation/dto/response/QueryReplyResponse.java
@@ -0,0 +1,32 @@
+package kr.co.souso.souso.domain.comment.presentation.dto.response;
+
+import io.swagger.annotations.ApiModelProperty;
+import kr.co.souso.souso.domain.user.presentation.dto.response.AuthorResponse;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class QueryReplyResponse {
+
+    @ApiModelProperty(value = "댓글 작성자")
+    private AuthorResponse author;
+
+    @ApiModelProperty(value = "댓글 고유 ID", example = "2")
+    private Long commentId;
+
+    @ApiModelProperty(value = "글 작성자인지 유무", example = "true")
+    private Boolean isFeedOwner;
+
+    @ApiModelProperty(value = "답글 작성자인지 유무", example = "true")
+    private Boolean isCommentOwner;
+
+    @ApiModelProperty(value = "내용", example = "만남의 광장입니다~")
+    private String content;
+
+    @ApiModelProperty(value = "작성일자", example = "2022-11-22T23:21:54")
+    private LocalDateTime createdAt;
+
+}
