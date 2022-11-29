167ca96667ddc5245b8bee39159af7631d6a726f
dkswnkk <jjoy9810@gmail.com>
2022-11-29 16:24:23 +0900

    refactor: comment 관련 네이밍 변경

--- src/main/java/kr/co/souso/souso/domain/comment/presentation/dto/response/QueryCommentDetailsResponse.java
+++ src/main/java/kr/co/souso/souso/domain/comment/presentation/dto/response/QueryCommentDetailsResponse.java
@@ -6,27 +6,31 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.*;
 
 @Getter
 @Builder
-public class QueryReplyResponse {
+public class QueryCommentDetailsResponse {
 
-    @ApiModelProperty(value = "답글 작성자")
+    @ApiModelProperty(value = "댓글 작성자")
     private AuthorResponse author;
 
-    @ApiModelProperty(value = "답글 고유 ID", example = "2")
+    @ApiModelProperty(value = "댓글 고유 ID", example = "2")
     private Long commentId;
 
     @ApiModelProperty(value = "글 작성자인지 유무", example = "true")
     private Boolean isFeedOwner;
 
-    @ApiModelProperty(value = "답글 작성자인지 유무", example = "true")
+    @ApiModelProperty(value = "댓글 작성자인지 유무", example = "true")
     private Boolean isCommentOwner;
 
-    @ApiModelProperty(value = "내용", example = "만남의 광장입니다~")
+    @ApiModelProperty(value = "내용", example = "여기가 어디인가요?")
     private String content;
 
     @ApiModelProperty(value = "작성일자", example = "2022-11-22T23:21:54")
     private LocalDateTime createdAt;
+
+    @ApiModelProperty(value = "답글")
+    List<QueryReplyDetailsResponse> reply;
 
 }
