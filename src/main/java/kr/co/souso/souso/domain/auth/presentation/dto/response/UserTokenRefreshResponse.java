86a968bfbb41641f00f21f4fb049fb05ea26018c
Sangjin <sangjin170202@naver.com>
2022-11-30 20:54:41 +0900

    feat(#120): UserTokenRefreshResponse 추가

---
+++ src/main/java/kr/co/souso/souso/domain/auth/presentation/dto/response/UserTokenRefreshResponse.java
@@ -0,0 +1,24 @@
+package kr.co.souso.souso.domain.auth.presentation.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.ZonedDateTime;
+
+@Getter
+@Builder
+public class UserTokenRefreshResponse {
+
+    @ApiModelProperty(value = "accesToken")
+    private final String accessToken;
+
+    @ApiModelProperty(value = "토큰 유효일자")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private final ZonedDateTime expiredAt;
+
+    @ApiModelProperty(value = "refreshToken")
+    private final String refreshToken;
+
+}
