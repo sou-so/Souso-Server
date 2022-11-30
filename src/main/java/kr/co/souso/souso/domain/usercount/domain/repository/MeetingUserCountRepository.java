9693e011f24aee4f0300efb0eeb4e99ccc2345ab
Sangjin <sangjin170202@naver.com>
2022-11-30 15:08:52 +0900

    feat(#84): MeetingUserCountRepository 추가

---
+++ src/main/java/kr/co/souso/souso/domain/usercount/domain/repository/MeetingUserCountRepository.java
@@ -0,0 +1,8 @@
+package kr.co.souso.souso.domain.usercount.domain.repository;
+
+import kr.co.souso.souso.domain.usercount.domain.MeetingUserCount;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MeetingUserCountRepository extends CrudRepository<MeetingUserCount, Long> {
+
+}
