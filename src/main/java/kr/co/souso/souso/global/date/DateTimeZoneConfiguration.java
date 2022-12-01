f7d6d59dd8b5b1b139b35bef0facad650075dc77
Sangjin <sangjin170202@naver.com>
2022-12-02 07:02:12 +0900

    feat(#127): DateTimeZoneConfiguration 추가

---
+++ src/main/java/kr/co/souso/souso/global/date/DateTimeZoneConfiguration.java
@@ -0,0 +1,15 @@
+package kr.co.souso.souso.global.date;
+
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@Configuration
+public class DateTimeZoneConfiguration {
+
+    @PostConstruct
+    public void setup() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+}
