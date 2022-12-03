b939668bc87c5f8a90f136f7bcf08fdd354b5b35
Sangjin <sangjin170202@naver.com>
2022-12-03 18:37:29 +0900

    delete: 불필요한 import문 삭제

--- src/main/java/kr/co/souso/souso/domain/feed/domain/repository/FeedRepositoryCustom.java
+++ src/main/java/kr/co/souso/souso/domain/feed/domain/repository/FeedRepositoryCustom.java
@@ -3,7 +3,6 @@ package kr.co.souso.souso.domain.feed.domain.repository;
 
 import kr.co.souso.souso.domain.feed.domain.repository.vo.FeedConditionVO;
 import kr.co.souso.souso.domain.feed.domain.repository.vo.FeedDetailsVO;
-import kr.co.souso.souso.global.enums.SortPageType;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
