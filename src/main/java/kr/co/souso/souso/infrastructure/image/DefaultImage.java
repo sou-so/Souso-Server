6115366d03be646c0427608f4908ffbc5cac2bbb
dkswnkk <jjoy9810@gmail.com>
2022-12-04 08:57:12 +0900

    refactor(#145): 기본 프로필이미지 주소 변경

--- src/main/java/kr/co/souso/souso/infrastructure/image/DefaultImage.java
+++ src/main/java/kr/co/souso/souso/infrastructure/image/DefaultImage.java
@@ -6,6 +6,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class DefaultImage {
 
-    public static final String USER_PROFILE_IMAGE = "https://souso-bucket.s3.ap-northeast-2.amazonaws.com/logo.svg";
+    public static final String USER_PROFILE_IMAGE = "https://souso-bucket.s3.ap-northeast-2.amazonaws.com/defaultProfileImage.svg";
 
 }
