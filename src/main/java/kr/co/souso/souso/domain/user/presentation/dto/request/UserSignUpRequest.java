4715ccad610756f53603f0958fd56e1747ce2813
Sangjin <sangjin170202@naver.com>
2022-12-04 12:13:11 +0900

    fix(#148): 띄어쓰기 피드백 반영

--- src/main/java/kr/co/souso/souso/domain/user/presentation/dto/request/UserSignUpRequest.java
+++ src/main/java/kr/co/souso/souso/domain/user/presentation/dto/request/UserSignUpRequest.java
@@ -22,7 +22,7 @@ public class UserSignUpRequest {
     @ApiModelProperty(value = "password", example = "Ab12345!")
     @NotBlank(message = "password는 Null, 공백, 띄어쓰기를 허용하지 않습니다.")
     @Pattern(regexp = "(?=.*[a-z])(?=.*[0-9])[a-zA-Z0-9]{8,20}$",
-            message = "password는 소문자, 숫자가 포함되어야 합니다. 대문자는 사용가능하지만 필수조건은 아닙니다.")
+            message = "password는 소문자, 숫자가 포함되어야 합니다. 대문자는 사용 가능하지만 필수조건은 아닙니다.")
     private String password;
 
     @ApiModelProperty(value = "name", example = "홍길동")
