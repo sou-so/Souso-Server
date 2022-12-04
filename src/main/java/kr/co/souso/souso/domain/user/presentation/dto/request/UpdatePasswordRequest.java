68ab3803b52c0d4dca50fac8bc15136866f69504
Sangjin <sangjin170202@naver.com>
2022-12-04 12:08:56 +0900

    fix(#148): 비밀번호 변경 password Regex 수정

--- src/main/java/kr/co/souso/souso/domain/user/presentation/dto/request/UpdatePasswordRequest.java
+++ src/main/java/kr/co/souso/souso/domain/user/presentation/dto/request/UpdatePasswordRequest.java
@@ -13,16 +13,14 @@ public class UpdatePasswordRequest {
 
     @ApiModelProperty(value = "기존 비밀번호", example = "souso1234#")
     @NotBlank(message = "password는 Null, 공백, 띄어쓰기를 허용하지 않습니다.")
-    @Pattern(regexp = "(?=.*[a-z])(?=.*[0-9])(?=.*[!#$%&'()*+,./:;<=>?@＼^_`{|}~])[a-zA-Z0-9!#$%&'()*+,./:;" +
-            "<=>?@＼^_`{|}~]{8,60}$",
-            message = "password는 소문자, 숫자, 특수문자가 포함되고 8자 이상이어야 합니다.")
+    @Pattern(regexp = "(?=.*[a-z])(?=.*[0-9])[a-zA-Z0-9]{8,20}$",
+            message = "password는 소문자, 숫자가 포함되어야 합니다. 대문자는 사용가능하지만 필수조건은 아닙니다.")
     private String password;
 
     @ApiModelProperty(value = "새로운 비밀번호", example = "souso12345!")
     @NotBlank(message = "new_password는 Null, 공백, 띄어쓰기를 허용하지 않습니다.")
-    @Pattern(regexp = "(?=.*[a-z])(?=.*[0-9])(?=.*[!#$%&'()*+,./:;<=>?@＼^_`{|}~])[a-zA-Z0-9!#$%&'()*+,./:;" +
-            "<=>?@＼^_`{|}~]{8,60}$",
-            message = "new_password는 소문자, 숫자, 특수문자가 포함되고 8자 이상이어야 합니다.")
+    @Pattern(regexp = "(?=.*[a-z])(?=.*[0-9])[a-zA-Z0-9]{8,20}$",
+            message = "password는 소문자, 숫자가 포함되어야 합니다. 대문자는 사용가능하지만 필수조건은 아닙니다.")
     private String newPassword;
 
 }
