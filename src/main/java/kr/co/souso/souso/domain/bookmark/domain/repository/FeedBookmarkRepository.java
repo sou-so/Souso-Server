29f65e6006bef7e869de2bee763f8aa4aa75a331
Sangjin <sangjin170202@naver.com>
2022-12-07 21:52:48 +0900

    fix(#162): code convention 통일

--- src/main/java/kr/co/souso/souso/domain/bookmark/domain/repository/FeedBookmarkRepository.java
+++ src/main/java/kr/co/souso/souso/domain/bookmark/domain/repository/FeedBookmarkRepository.java
@@ -13,4 +13,5 @@ public interface FeedBookmarkRepository extends JpaRepository<FeedBookmark, Feed
     Optional<FeedBookmark> findByFeedAndUser(Feed feed, User user);
 
     Long countByUser(User user);
+
 }
