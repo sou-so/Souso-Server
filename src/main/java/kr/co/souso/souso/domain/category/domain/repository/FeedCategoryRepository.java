4b1438de28b0fc9c617cafffb2450fd28973a780
Sangjin <sangjin170202@naver.com>
2022-12-07 21:53:06 +0900

    fix(#162): code convention 통일 및 불필요한 import문 삭제

--- src/main/java/kr/co/souso/souso/domain/category/domain/repository/FeedCategoryRepository.java
+++ src/main/java/kr/co/souso/souso/domain/category/domain/repository/FeedCategoryRepository.java
@@ -2,11 +2,10 @@ package kr.co.souso.souso.domain.category.domain.repository;
 
 import kr.co.souso.souso.domain.category.domain.FeedCategory;
 import kr.co.souso.souso.domain.category.domain.FeedCategoryId;
-import kr.co.souso.souso.domain.feed.domain.Feed;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 public interface FeedCategoryRepository extends JpaRepository<FeedCategory, FeedCategoryId>{
 
     FeedCategory findFeedCategoryByFeedId(Long feedId);
+
 }
